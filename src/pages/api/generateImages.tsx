a2bb34816e1694af809f1bc92840907640e7052e
Vkiti <viktor.skific@gmail.com>
2023-01-27 21:26:05 +0100

    Remove .env

--- src/pages/api/generateImages.tsx
+++ src/pages/api/generateImages.tsx
@@ -1,19 +1,23 @@
-import axios from "axios";
-import type { NextApiRequest, NextApiResponse } from "next";
+import axios from 'axios';
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
+    req: NextApiRequest,
+    res: NextApiResponse
 ) {
-  const { body } = req;
-  try {
-    const response = await axios.post(process.env.CREATE_IMAGE_ENDPOINT!, body, {
-      headers: { authorization: `Bearer ${process.env.OPENAI_KEY}` },
-    });
+    const { body } = req;
+    try {
+        const response = await axios.post(
+            process.env.CREATE_IMAGE_ENDPOINT!,
+            body,
+            {
+                headers: { authorization: `Bearer ${process.env.OPENAI_KEY}` },
+            }
+        );
 
-    res.status(200).json(response.data);
-  } catch (error) {
-    console.log(error);
-    res.status(400).json({ error });
-  }
+        res.status(200).json(response.data);
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({ error });
+    }
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,96 +2,162 @@ import { buttons, UserInput } from '@/components/userInput';
 import useAutosizeTextArea from '@/textArea/AutoResizeTextArea';
 import { imagesType } from '@/Types/Types';
 import axios from 'axios';
-import Head from 'next/head'
+import Head from 'next/head';
 import Image from 'next/image';
 import React, { useEffect, useRef, useState } from 'react';
 
 export default function Home() {
-  const [generatedImage, setGeneratedImage] = useState<imagesType[] | null>(null);
-  const [prompt, setPrompt] = useState("")
-  const [promptError, setPromptError] = useState(false);
-  const [numberOfImages, setNumberOfImages] = useState(1)
-  const [numberOfImagesError, setNumberOfImagesError] = useState(false)
-  const [selectedSizeOfImage, setSelectedSizeOfImage] = useState("256x256")
-  const [loading, setLoading] = useState(false)
-  const textAreaRef = useRef<HTMLTextAreaElement>(null);
+    const [generatedImage, setGeneratedImage] = useState<imagesType[] | null>(
+        null
+    );
+    const [prompt, setPrompt] = useState('');
+    const [promptError, setPromptError] = useState(false);
+    const [numberOfImages, setNumberOfImages] = useState(1);
+    const [numberOfImagesError, setNumberOfImagesError] = useState(false);
+    const [selectedSizeOfImage, setSelectedSizeOfImage] = useState('256x256');
+    const [loading, setLoading] = useState(false);
+    const textAreaRef = useRef<HTMLTextAreaElement>(null);
 
-  useAutosizeTextArea(textAreaRef.current, prompt);
+    useAutosizeTextArea(textAreaRef.current, prompt);
 
-  const handlePromptChange = (evt: React.ChangeEvent<HTMLTextAreaElement>) => {
-    const val = evt.target?.value;
+    const handlePromptChange = (
+        evt: React.ChangeEvent<HTMLTextAreaElement>
+    ) => {
+        const val = evt.target?.value;
 
-    setPrompt(val);
-  };
+        setPrompt(val);
+    };
 
-  const handleNumberOfImagesChange = (event:React.ChangeEvent<HTMLInputElement>) => {
-    setNumberOfImages(Number(event.target.value))
-    if(Number(event.target.value ) > 10) {
-      setNumberOfImagesError(true)
-    }
-  }
+    const handleNumberOfImagesChange = (
+        event: React.ChangeEvent<HTMLInputElement>
+    ) => {
+        if (Number(event.target.value) > 10) {
+            setNumberOfImagesError(true);
+            return;
+        }
+        setNumberOfImages(Number(event.target.value));
+    };
 
-  const loadingWidget = () => {
-    if(prompt){
-      setLoading(true)
-    }
-  }
- 
-  const generateImage = async () => {
-    if (!prompt) {
-      setPromptError(true);
-      return;
-    }
-    const response = await axios.post("/api/generateImages", {
-      prompt,
-      n: numberOfImages,
-      size: selectedSizeOfImage
-    });
-    setGeneratedImage(response.data.data);
-    setLoading(false)
-  };
-  
-  useEffect(() => {
-    const promptTimer = setTimeout(() => promptError && setPromptError(false), 3000);
-    const numberTimer = setTimeout(() => numberOfImagesError && setNumberOfImagesError(false), 3000)
+    const loadingWidget = () => {
+        if (prompt) {
+            setLoading(true);
+        }
+    };
 
-    return () => clearTimeout(promptTimer);
-  }, [promptError, numberOfImagesError]);
+    const generateImage = async () => {
+        if (!prompt) {
+            setPromptError(true);
+            return;
+        }
+        const response = await axios.post('/api/generateImages', {
+            prompt,
+            n: numberOfImages,
+            size: selectedSizeOfImage,
+        });
 
-  return (
-    <>
-      <Head>
-        <title>Generate images project</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <main>
-        <div className='flex flex-col w-full h-screen gap-5 justify-center items-center p-[20px]'>
-        <p className='uppercase text-xl font-thin font-mono'>Image Generator</p>
-        <UserInput prompt={prompt} handlePromptChange={handlePromptChange} handleNumberOfImagesChange={handleNumberOfImagesChange} textAreaRef={textAreaRef} promptError={promptError}/>
-        {promptError && (
-        <div className="text-red-400">Please describe the picture you want to generate.</div>
-      )}
-        {numberOfImagesError && (
-          <p className="text-red-400">maximum amount of pictures generated can be 10</p>
-        )}
-        <div className='flex flex-row gap-4'>
-            {buttons.map((button, index) => (
-                <button key={index} onClick={() => setSelectedSizeOfImage(button)} className={`${selectedSizeOfImage === button ?  "bg-blue-200 scale-125" : ""} text-black bg-blue-200 px-4 py-1 text-center rounded-full text-sm sizeButton`}>{button}</button>
-                ))}
-        </div>
-        <button onClick={() => {
-          generateImage();
-          loadingWidget()
-        }} className="generateButton rounded-full px-[25px] py-[5px] bg-blue-400 font-bold uppercase text-xl">Generate!</button>
-        {loading && (
-          <div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div>)}
-        <div className='flex justify-between flex-row flex-wrap gap-6'>
-          {generatedImage ? generatedImage.map((image) => (
-            <Image src={image.url} alt={prompt} width={300} height={300} key={image.url} className="border border-black"/>
-            )) : null}
-            </div>
-        </div>
-      </main>
-    </>
-  )
+        setGeneratedImage(response.data.data);
+        setLoading(false);
+    };
+
+    useEffect(() => {
+        const promptTimer = setTimeout(
+            () => promptError && setPromptError(false),
+            3000
+        );
+
+        return () => clearTimeout(promptTimer);
+    }, [promptError]);
+
+    useEffect(() => {
+        const numberTimer = setTimeout(
+            () => numberOfImagesError && setNumberOfImagesError(false),
+            3000
+        );
+
+        return () => clearTimeout(numberTimer);
+    }, [numberOfImagesError]);
+
+    console.log(numberOfImages);
+
+    return (
+        <>
+            <Head>
+                <title>Generate images project</title>
+                <meta
+                    name='viewport'
+                    content='width=device-width, initial-scale=1'
+                />
+            </Head>
+            <main>
+                <div className='flex flex-col w-full h-screen gap-5 justify-center items-center p-[20px]'>
+                    <p className='uppercase text-xl font-thin font-mono'>
+                        Image Generator
+                    </p>
+                    <UserInput
+                        prompt={prompt}
+                        handlePromptChange={handlePromptChange}
+                        handleNumberOfImagesChange={handleNumberOfImagesChange}
+                        textAreaRef={textAreaRef}
+                        promptError={promptError}
+                    />
+                    {promptError && (
+                        <div className='text-red-400'>
+                            Please describe the picture you want to generate.
+                        </div>
+                    )}
+                    {numberOfImagesError && (
+                        <p className='text-red-400'>
+                            maximum amount of pictures generated can be 10
+                        </p>
+                    )}
+                    <div className='flex flex-row gap-4'>
+                        {buttons.map((button, index) => (
+                            <button
+                                key={index}
+                                onClick={() => setSelectedSizeOfImage(button)}
+                                className={`${
+                                    selectedSizeOfImage === button
+                                        ? 'bg-blue-200 scale-125'
+                                        : ''
+                                } text-black bg-blue-200 px-4 py-1 text-center rounded-full text-sm sizeButton`}
+                            >
+                                {button}
+                            </button>
+                        ))}
+                    </div>
+                    <button
+                        onClick={() => {
+                            generateImage();
+                            loadingWidget();
+                        }}
+                        className='generateButton rounded-full px-[25px] py-[5px] bg-blue-400 font-bold uppercase text-xl'
+                    >
+                        Generate!
+                    </button>
+                    {loading && (
+                        <div className='lds-ellipsis'>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                        </div>
+                    )}
+                    <div className='flex justify-between flex-row flex-wrap gap-6'>
+                        {generatedImage
+                            ? generatedImage.map((image) => (
+                                  <Image
+                                      src={image.url}
+                                      alt={prompt}
+                                      width={300}
+                                      height={300}
+                                      key={image.url}
+                                      className='border border-black'
+                                  />
+                              ))
+                            : null}
+                    </div>
+                </div>
+            </main>
+        </>
+    );
 }
